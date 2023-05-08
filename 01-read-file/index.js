1aca9cb035f06408a8306b20849fc7d68a18b756
kak-maxim <komkovmix@gmail.com>
2023-05-08 20:36:53 +0300

    feat: end first task

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'text.txt');
+const readFileStream = fs.createReadStream(filePath, 'utf8');
+readFileStream.on('data', (chunk) => {
+    console.log(chunk);
+});
