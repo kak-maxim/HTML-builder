4cd34265cf70a1a556ec8dfb218ef155f142aebf
kak-maxim <komkovmix@gmail.com>
2023-05-08 21:19:48 +0300

    feat: add second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin } = require('node:process');
+const route = path.join(__dirname, '.\\text.txt');
+const fileStream = fs.createWriteStream(route, 'utf-8');
+
+console.log('hi pls enter your text\n');
+
+stdin.on('data', data => {
+  if (data.toString().trim() === 'exit') {
+      console.log("bye");
+      process.exit();
+  }
+  else {
+    fileStream.write(data, 'utf-8');
+  }
+});
+
+process.addListener('SIGINT', () => {
+  process.exit();
+});
